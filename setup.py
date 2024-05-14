eeee4cb78dfd0c336e213953787e28e581118ad2
江离 <113815984+jiangli0618@users.noreply.github.com>
2024-05-14 16:31:19 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r",encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='cwslib1',
+    name='cwslib',
     version='1.0.0',
     author='jiangli',
     author_email='1329084163@qq.com',
